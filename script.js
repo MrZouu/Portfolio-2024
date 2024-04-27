f2855a925f946aa9e9c0671c961acec2aae69687
MrZouu <lor3nsom@gmail.com>
2024-04-27 22:29:44 +0200

    homepage

--- script.js
+++ script.js
@@ -7,3 +7,31 @@ variables.*/
   menu.classList.toggle('open');
   icon.classList.toggle('open');
 }
+
+
+// Avoir la barre de navigation qui se cache en scrollant vers le bas et qui réapparait en scrollant vers le haut
+// Sélectionne la barre de navigation
+const nav = document.getElementById('desktop-nav');
+
+// Stocke la position actuelle du scroll
+let lastScrollTop = 0;
+
+// Fonction pour détecter le mouvement du scroll
+window.addEventListener('scroll', () => {
+  // Récupère la nouvelle position du scroll
+  const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+
+  // Vérifie la direction du scroll et si le scroll est suffisamment important pour être pris en compte
+  if (scrollTop > lastScrollTop && scrollTop > nav.clientHeight) {
+    // Scroll vers le bas : masque la barre de navigation
+    nav.style.transition = 'top 0.5s';
+    nav.style.top = `-${nav.clientHeight}px`;
+  } else {
+    // Scroll vers le haut : affiche la barre de navigation
+    nav.style.transition = 'top 0.5s';
+    nav.style.top = '0';
+  }
+
+  // Met à jour la position du scroll précédente
+  lastScrollTop = scrollTop;
+});
